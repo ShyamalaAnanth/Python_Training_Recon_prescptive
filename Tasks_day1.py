2fbb2e234b0615017f6978b6f419897198fe939e
ShyamalaAnanth <shyamala.ananthanarayanan@atyeti.com>
2025-09-25 16:03:54 +0530

    Tasks_day1.py

--- Tasks_day1.py
+++ Tasks_day1.py
@@ -1,1 +1,25 @@
+#Sum of two numbers
+x=int(input("Enter num1 to sum:"));
+y=int(input("Enter num2 to sum:"));
+print("Sum of the two numbers is:",x+y);
+
+#Area of square
+side=int(input("enter value for side to continue:"));
+area=side*side;
+print("Side of the square is",side);
+print("Area of the square is",area);
+
+#Average of two floating point numbers
+x=float(input("Enter num1 to find average:"));
+y=float(input("Enter num2 to find average:"));
+print("Average of the two floating point numbers is:",(x+y)/2);
+
+#Check if number is greater than or equal to another and print True or False
+x=int(input("Enter num x to continue:"));
+y=int(input("Enter num y to continue:"));
+if x >= y:
+    print("True x is greater than y");
+else:
+    print ("False y is greater than x")
+
 
